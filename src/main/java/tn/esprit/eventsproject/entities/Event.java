f28e67c63cbfcca65773ea85f5a4afc36c869dd4
sahbikhalfaoui <114394506+sahbikhalfaoui@users.noreply.github.com>
2024-12-11 05:56:16 +0100

    Update Event.java

--- src/main/java/tn/esprit/eventsproject/entities/Event.java
+++ src/main/java/tn/esprit/eventsproject/entities/Event.java
@@ -12,6 +12,7 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class Event implements Serializable {
